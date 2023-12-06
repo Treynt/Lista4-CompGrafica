7c669e45fcb707b7016411977212e1051cda04dd
Silvio Goncalves <silvio.goncalves@aluno.uece.br>
2023-12-06 14:11:32 -0300

    Comp Grafica

---
+++ Tela.py
@@ -0,0 +1,46 @@
+import glfw
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+def draw_line(start1, end1, start2, end2):
+    glColor3f(0.0, 0.0, 1.0)
+    glBegin(GL_LINE_STRIP)
+    glVertex2f(start1, end1)
+    glVertex2f(start2, end2)
+    glEnd()
+
+def main():
+#Initializing glfw library
+    if not glfw.init():
+        raise Exception ("glfw can not be initialized")
+
+    #creating a window
+    window = glfw.create_window(1280, 720, "My OpenGL window", None, None)
+
+    #check if window was created
+    if not window:
+        glfw.terminate()
+        raise Exception ("glfw window can not be created")
+
+    #Set window's position
+    glfw.set_window_pos(window, 400, 200)
+
+    #Make the context current
+    glfw.make_context_current(window)
+
+    glClearColor(0, 0.1, 0.1, 1)
+
+    #The main application loop
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glfw.swap_buffers(window)
+
+    #terminate glfw, free up allocated resources
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()  
